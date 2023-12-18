1fe350ebf0736afb10f4cc6fb4c22c67a61b0cd9
cathKitten <cat.schoefmann@drei.at>
2023-12-18 23:15:28 +0100

    Add ClothConfig dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,8 @@ group = property("maven_group")!!
 version = property("mod_version")!!
 
 repositories {
-    // Add repositories to retrieve artifacts from in here.
-    // You should only use this when depending on other mods because
-    // Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
-    // See https://docs.gradle.org/current/userguide/declaring_repositories.html
-    // for more information about repositories.
+    maven("https://maven.shedaniel.me/")
+    maven("https://maven.terraformersmc.com/releases/")
 }
 
 dependencies {
@@ -23,10 +20,14 @@ dependencies {
 
     modImplementation("net.fabricmc:fabric-language-kotlin:${property("fabric_kotlin_version")}")
     modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
+
+    modApi("me.shedaniel.cloth:cloth-config-fabric:${property("cloth_config_version")}") {
+        exclude("net.fabricmc.fabric-api")
+    }
 }
 
-tasks {
 
+tasks {
     processResources {
         inputs.property("version", project.version)
         filesMatching("fabric.mod.json") {
@@ -70,7 +71,6 @@ java {
     // If you remove this line, sources will not be generated.
     withSourcesJar()
 }
-
 
 
 // configure the maven publication
